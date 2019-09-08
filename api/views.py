96202b6555c2d98ee7b17f3fe95cf8ce730e0bf5
hvl5451 <hvl5451@psu.edu>
2019-09-08 02:28:48 -0400

    afasf

--- api/views.py
+++ api/views.py
@@ -48,7 +48,7 @@ def update_pill_data(request):
 def load_pill_data(request):
     # data = list(map(lambda x: x.__dict__, PillDetails.objects.all()))
     # data = serializers.dataSerializer(PillDetails.objects.all(), many=True)
-    data = serializers.serialize('json', PillDetails.objects.all())
+    data = list(map(lambda x: x.fields, serializers.serialize('json', PillDetails.objects.all())))
     print(data, flush=True)
     return HttpResponse(data, content_type=json)
 
